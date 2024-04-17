b71dc677522f02576c853f15236ecf0a1015e230
JinmingZhuang <m13688033757@163.com>
2024-04-17 21:11:58 +0000

    [Transform][Tiling] Update Tiling for User Specified Factors

---
+++ example/add.mlir
@@ -0,0 +1,13 @@
+module {
+  func.func @vectoradd(%arg0: memref<64xf32>, %arg1: memref<64xf32>, 
+                       %arg2: memref<64xf32>) {
+    affine.for %arg3 = 0 to 64 {
+        %0 = affine.load %arg1[%arg3] : memref<64xf32>
+        %1 = affine.load %arg2[%arg3] : memref<64xf32>
+        %2 = arith.addf %0, %1 : f32
+        affine.store %2, %arg0[%arg3] : memref<64xf32>
+    }
+    return
+  }
+}
+
--- lib/Transform/AriesTiling.cpp
+++ lib/Transform/AriesTiling.cpp
@@ -2,19 +2,55 @@
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Affine/LoopUtils.h"
 #include "mlir/Analysis/Liveness.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 
 using namespace mlir;
+using namespace mlir::affine;
 using namespace aries;
+using namespace func;
 
 namespace {
+
 struct AriesTiling : public AriesTilingBase<AriesTiling> {
     void runOnOperation() override {
         auto mod = getOperation();
-        if (0) {
-          signalPassFailure();
-        }
-    }
+        unsigned defaultTileSizes = 32;
+        SmallVector<unsigned,6> optTileSizes;
 
+        // Receive Specified Tiling Factors
+        optTileSizes.clear();
+        if (TileSizes.size() > 0) {
+            // Initialize tile sizes from the command line.
+            for (unsigned i = 0; i < TileSizes.size(); ++i) {
+              optTileSizes.push_back(TileSizes[i]);
+            }
+        }
+
+
+        mod->walk([&](FuncOp op){
+            std::vector<SmallVector<AffineForOp, 6>> bands;
+            getTileableBands(op, &bands);
+            
+
+            for (auto band : bands) {
+                // Set the default tiling fatctor
+                SmallVector<unsigned,6> tileSizes(band.size(),defaultTileSizes);
+
+                // Assign received tiling factors to the tilable loop bands
+                for (unsigned i = 0; i < std::min(band.size(),optTileSizes.size()); ++i) {
+                  tileSizes[i] = optTileSizes[i];
+                }
+                
+                SmallVector<AffineForOp,6> tileBand;
+
+                if (failed(tilePerfectlyNested(band, tileSizes, &tileBand)))
+                    return signalPassFailure();
+                
+            }
+        });
+        
+    }
 };
 } // namespace
 
