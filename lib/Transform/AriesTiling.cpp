97da8275adb714eeb67b4206aedec5bf38cf5efe
JinmingZhuang <m13688033757@163.com>
2024-04-19 17:31:55 +0000

    [Transform] Update AriesTiling Pass

--- lib/Transform/AriesTiling.cpp
+++ lib/Transform/AriesTiling.cpp
@@ -1,7 +1,6 @@
 #include "aries/Transform/Passes.h"
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
 #include "mlir/Dialect/Affine/LoopUtils.h"
-#include "mlir/Analysis/Liveness.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 
@@ -10,15 +9,51 @@ using namespace mlir::affine;
 using namespace aries;
 using namespace func;
 
+namespace mlir {
+namespace aries {
+
+
+  bool applyLoopTiling(ModuleOp mod, unsigned defaultTileSizes,SmallVector<unsigned,6> &optTileSizes){
+    auto b = OpBuilder(mod);
+    
+    for (auto &op : mod.getBody()->getOperations()) {
+      if (auto func = dyn_cast<FuncOp>(op)) {
+        func->setAttr("top_func", b.getUnitAttr());
+        std::vector<SmallVector<AffineForOp, 6>> bands;
+        getTileableBands(func, &bands);
+        
+        for (auto band : bands) {
+            // Set the default tiling fatctor
+            SmallVector<unsigned,6> tileSizes(band.size(),defaultTileSizes);
+
+            // Assign received tiling factors to the tilable loop bands
+            for (unsigned i = 0; i < std::min(band.size(),optTileSizes.size()); ++i) {
+              tileSizes[i] = optTileSizes[i];
+            }
+            
+            // Call Affine tiling functions for perfectly nested loops
+            SmallVector<AffineForOp,6> tileBand;
+            if (failed(tilePerfectlyNested(band, tileSizes, &tileBand)))
+                return false;
+        }
+      }
+    }
+
+    return true;
+  }
+
+} // namespace aries
+} // namespace mlir
+
 namespace {
 
 struct AriesTiling : public AriesTilingBase<AriesTiling> {
     void runOnOperation() override {
-        auto mod = getOperation();
+        auto mod = dyn_cast<ModuleOp>(getOperation());
         unsigned defaultTileSizes = 32;
-        SmallVector<unsigned,6> optTileSizes;
-
+        
         // Receive Specified Tiling Factors
+        SmallVector<unsigned,6> optTileSizes;
         optTileSizes.clear();
         if (TileSizes.size() > 0) {
             // Initialize tile sizes from the command line.
@@ -27,29 +62,8 @@ struct AriesTiling : public AriesTilingBase<AriesTiling> {
             }
         }
 
-
-        mod->walk([&](FuncOp op){
-            std::vector<SmallVector<AffineForOp, 6>> bands;
-            getTileableBands(op, &bands);
-            
-
-            for (auto band : bands) {
-                // Set the default tiling fatctor
-                SmallVector<unsigned,6> tileSizes(band.size(),defaultTileSizes);
-
-                // Assign received tiling factors to the tilable loop bands
-                for (unsigned i = 0; i < std::min(band.size(),optTileSizes.size()); ++i) {
-                  tileSizes[i] = optTileSizes[i];
-                }
-                
-                SmallVector<AffineForOp,6> tileBand;
-
-                if (failed(tilePerfectlyNested(band, tileSizes, &tileBand)))
-                    return signalPassFailure();
-                
-            }
-        });
-        
+        if(!applyLoopTiling(mod, defaultTileSizes,optTileSizes))
+          return signalPassFailure();
     }
 };
 } // namespace
