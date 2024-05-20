5a15a960f048b460b6c5e336201cab619ce62fdf
JinmingZhuang <m13688033757@163.com>
2024-05-20 03:16:59 +0000

    [Dialect][ADFOps] Create ADF operations

--- lib/Dialect/ADF/ADFDialect.cpp
+++ lib/Dialect/ADF/ADFDialect.cpp
@@ -3,6 +3,7 @@
 #include "llvm/include/llvm/ADT/TypeSwitch.h"
 #include "aries/Dialect/ADF/ADFDialect.h"
 #include "aries/Dialect/ADF/ADFTypes.h"
+#include "aries/Dialect/ADF/ADFOps.h"
 
 using namespace mlir;
 using namespace aries;
@@ -12,10 +13,16 @@ using namespace mlir::aries::adf;
 #include "aries/Dialect/ADF/ADFDialect.cpp.inc"
 #define GET_TYPEDEF_CLASSES
 #include "aries/Dialect/ADF/ADFTypes.cpp.inc"
+#define GET_OP_CLASSES
+#include "aries/Dialect/ADF/ADFOps.cpp.inc"
 
 void ADFDialect::initialize() {
- addTypes<
+  addTypes<
 #define GET_TYPEDEF_LIST
 #include "aries/Dialect/ADF/ADFTypes.cpp.inc"
       >();
+  addOperations<
+#define GET_OP_LIST
+#include "aries/Dialect/ADF/ADFOps.cpp.inc"
+      >();  
 }
