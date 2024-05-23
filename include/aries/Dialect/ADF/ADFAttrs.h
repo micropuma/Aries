27b853958a1457a47c7ece6e3db02b8e0cad0097
Ubuntu <m13688033757@163.com>
2024-05-23 04:47:38 +0000

    Delete Unused Files

--- include/aries/Dialect/ADF/ADFAttrs.h
+++
@@ -1,9 +0,0 @@
-#ifndef ARIES_DIALECT_ARIES_ADF_ATTRS_H
-#define ARIES_DIALECT_ARIES_ADF_ATTRS_H
-
-#include "mlir/IR/BuiltinTypes.h"
-#include "mlir/IR/Dialect.h"
-
-#include "aries/Dialect/ADF/AIEEnums.h.inc"
-
-#endif//ARIES_DIALECT_ARIES_ADF_ATTRS_H
