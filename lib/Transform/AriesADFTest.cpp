92f87327f81664729f9d97cc5f99f3293d57c8af
Ubuntu <m13688033757@163.com>
2024-05-24 22:55:43 +0000

    [ADFDialect] Update IOPushOp & Corresponding Test Pass

--- lib/Transform/AriesADFTest.cpp
+++ lib/Transform/AriesADFTest.cpp
@@ -24,7 +24,8 @@ private:
   bool ADFTest (ModuleOp mod) {
     //PLIOCreate(mod);
     //GraphIOCreate(mod);
-    KernelIOCreate(mod);
+    //KernelIOCreate(mod);
+    IOPushOpCreate(mod);
     return true;
   }
 
@@ -51,6 +52,21 @@ private:
     builder.setInsertionPointToStart(mod.getBody());
     auto streamOp = builder.create<CreateKernelIOOp>(builder.getUnknownLoc(),StreamType::get(builder.getContext()), mlir::aries::adf::KernelIOName::Stream);
     auto cascadeOp = builder.create<CreateKernelIOOp>(builder.getUnknownLoc(),CascadeType::get(builder.getContext()), mlir::aries::adf::KernelIOName::Cascade);
+    return true;
+  }
+
+  bool IOPushOpCreate(ModuleOp mod){
+    auto builder = OpBuilder(mod);
+    builder.setInsertionPointToStart(mod.getBody());
+    auto plioOp = builder.create<CreateGraphIOOp>(builder.getUnknownLoc(),PLIOType::get(builder.getContext(), mlir::aries::adf::PortDir::In, (unsigned)mlir::aries::adf::PortWidth::Width128), mlir::aries::adf::GraphIOName::PLIO);
+    mlir::Type elementType = builder.getF32Type();
+    mlir::MemRefType memrefType = mlir::MemRefType::get({128}, elementType);
+    auto alloc = builder.create<mlir::memref::AllocOp>(builder.getUnknownLoc(), memrefType);
+    SmallVector<Value> src;
+    src.push_back(alloc.getResult());
+    SmallVector<Value> dst;
+    dst.push_back(plioOp.getResult());
+    auto ioPushOp = builder.create<IOPushOp>(builder.getUnknownLoc(),src,dst);
     return true;
   }
 
