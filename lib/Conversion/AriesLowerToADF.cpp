8ca48b3bad9bc3f96d5bc379c3987eee9635d409
JinmingZhuang <m13688033757@163.com>
2024-06-03 01:34:32 +0000

    [ADFDialect] Update io.push Op

--- lib/Conversion/AriesLowerToADF.cpp
+++ lib/Conversion/AriesLowerToADF.cpp
@@ -1,18 +1,48 @@
 #include "aries/Conversion/Passes.h"
 #include "aries/Dialect/ADF/ADFDialect.h"
+#include "aries/Transform/Utils.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
 
 using namespace mlir;
 using namespace aries;
 using namespace adf;
+using namespace mlir::memref;
+using namespace mlir::func;
 
 namespace {
 
 struct AriesLowerToADF : public AriesLowerToADFBase<AriesLowerToADF> {
 public:
   void runOnOperation() override {
-    MLIRContext *context = &getContext();
-    auto *module = getOperation();
+    auto mod = dyn_cast<ModuleOp>(getOperation());
+    StringRef topFuncName = "top_func";    
+  
+    if (!LowerToADF(mod,topFuncName))
+      signalPassFailure();
   }
+
+private:
+  bool LowerToADF(ModuleOp mod,StringRef topFuncName) {
+    auto builder = OpBuilder(mod);
+    FuncOp topFunc;
+
+    if(!topFind(mod, topFunc, topFuncName)){
+      topFunc->emitOpError("Top function not found\n");
+      return false;
+    }
+
+    ConversionTarget target(*mod.getContext());
+    RewritePatternSet patterns(mod.getContext());
+
+    target.addLegalOp<SubViewOp>();
+    target.addLegalOp<CopyOp>();
+    target.addLegalOp<AllocaOp>();
+    target.addLegalOp<DeallocOp>();
+    target.addLegalOp<CallOp>();
+  }
+
 };
 
 } // namespace
