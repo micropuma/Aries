8ad470c638bda17915dfbb580355f667ccaa0528
JinmingZhuang <m13688033757@163.com>
2024-07-08 23:27:43 +0000

    [Translation] Add the Single Kernel Emitter

--- lib/Translation/EmitKernelFunc.cpp
+++ lib/Translation/EmitKernelFunc.cpp
@@ -1,7 +1,5 @@
 #include "mlir/Tools/mlir-translate/Translation.h"
 #include "aries/Translation/Emitter.h"
-#include "aie/Targets/AIETargets.h"
-
 
 using namespace mlir;
 using namespace aries;
@@ -12,26 +10,23 @@ LogicalResult aries::emitKernelFunc(ModuleOp module, raw_ostream &os) {
     os << R"XXX(
 //===------------------------------------------------------------*- C++ -*-===//
 //
-// Automatically generated file for MLIR-AIE AIE kernel.
+// Automatically generated file for AIE kernel supported by Vitis Flow.
 //
 //===----------------------------------------------------------------------===//
 
-// #define __AIENGINE__ 1
-// #define NOCPP
-
-// #include <cardano.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-extern "C" {
+#include <aie_api/aie.hpp>
+#include <aie_api/aie_adf.hpp>
+#include <aie_api/utils.hpp>
+#include <adf/io_buffer/io_buffer.h>
+using namespace adf;
 
 )XXX";
 
-    if (failed(xilinx::aievec::translateAIEVecToCpp(func, false, os)))
+    if (failed(aries::emitAIEVecToCpp(func, false, os)))
       return failure();
-
-    os << "}\n";
   }
 
   return success();
--- lib/Translation/Emitter.cpp
+++ lib/Translation/Emitter.cpp
@@ -1,5 +1,12 @@
-#include "aries/Translation/Emitter.h"
 #include "mlir/Tools/mlir-translate/Translation.h"
+#include "mlir/Dialect/ControlFlow/IR/ControlFlow.h"
+#include "mlir/Dialect/DLTI/DLTI.h"
+#include "mlir/Dialect/EmitC/IR/EmitC.h"
+#include "mlir/Dialect/Index/IR/IndexDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/Math/IR/Math.h"
+#include "aries/Translation/Emitter.h"
+#include "mlir/Dialect/Vector/IR/VectorOps.h"
 #include "aries/Dialect/ADF/ADFDialect.h"
 #include "aie/Dialect/AIE/IR/AIEDialect.h"
 #include "aie/Dialect/AIEVec/IR/AIEVecOps.h"
@@ -44,11 +51,18 @@ void aries::registeremitKernelFuncTranslation() {
           mlir::memref::MemRefDialect,
           mlir::scf::SCFDialect,
           mlir::vector::VectorDialect,
-          xilinx::AIE::AIEDialect,
-          xilinx::aievec::AIEVecDialect
+          arith::ArithDialect,
+          emitc::EmitCDialect,
+          LLVM::LLVMDialect,
+          math::MathDialect,
+          cf::ControlFlowDialect,
+          DLTIDialect,
+          xilinx::aievec::AIEVecDialect,
+          index::IndexDialect
         >();
       });
 }
+
 
 void aries::registerAriesEmitters() {
   registerEmitADFCppTranslation();
