72022a0c2d0fa9d0ee3d24752c2030fd569fcd78
“JinmingZhuang” <m13688033757@163.com>
2024-08-06 00:57:21 +0000

    [Transform][AriesDMAToIO] Eliminate the IOPushOps that has the same src

--- lib/Dialect/ADF/ADFOps.cpp
+++ lib/Dialect/ADF/ADFOps.cpp
@@ -39,74 +39,6 @@ struct DuplicateElim : public OpRewritePattern<OpType> {
   }
 };
 
-
-bool IsdstTouched (IOPushOp op){
-  auto dst = op.getDst();
-  for(auto user : dst.getUsers()){
-    if(auto connectOp0 = dyn_cast<ConnectOp>(user)){
-      auto connectDst0 = connectOp0.getDst();
-      for(auto connectDstUser0 : connectDst0.getUsers()){
-        if(auto connectOp1 = dyn_cast<ConnectOp>(connectDstUser0)){
-          auto connectDst1 = connectOp1.getDst();
-          for(auto connectDstUser1 : connectDst1.getUsers()){
-            if(dyn_cast<IOPopOp>(connectDstUser1)){
-              return true;
-            }
-          }
-        }
-      }
-    }
-  }
-  return false;
-}
-
-//TODO: This should be a pass instead of a canonicalizer
-//Eliminate the pushop that share the same source and number of dst
-struct IOPushElim : public OpRewritePattern<IOPushOp> {
-  using OpRewritePattern<IOPushOp>::OpRewritePattern;
-
-  LogicalResult matchAndRewrite(IOPushOp op,
-                                PatternRewriter &rewriter) const override {
-                                  
-    SmallVector<IOPushOp> iopushOps;
-    IOPushOp firstUser = nullptr;
-    for (auto &user : op.getSrc().getUses()) {
-      if (auto otherOp = dyn_cast<IOPushOp>(user.getOwner())) {
-        //Get the first pushop that pushes the same memref slice
-        if (op.getSrc() == otherOp.getSrc() &&
-            op.getSrcOffsets() == otherOp.getSrcOffsets() &&
-            op.getSrcSizes() == otherOp.getSrcSizes() &&
-            op.getSrcStrides() == otherOp.getSrcStrides()){
-            if (!firstUser || otherOp->isBeforeInBlock(firstUser)) {
-              firstUser = otherOp;
-            }
-            iopushOps.push_back(otherOp);
-        }
-      }
-    }
-    bool flag = false;
-    for (auto iopushOp: iopushOps){
-      if(iopushOp == firstUser){
-        continue;
-      }else{
-        if(IsdstTouched(iopushOp)){
-          continue;
-        }else{
-          auto dst = iopushOp.getDst();
-          auto dstOther = firstUser.getDst();
-          dst.replaceAllUsesWith(dstOther);
-          iopushOp.erase();
-          flag = true;
-        }
-      }
-    }
-    if (flag)
-      return success();
-    else
-      return failure();
-  }
-};
-
 void CreateGraphIOOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                            MLIRContext *context) {
   results.add<DeadElim<CreateGraphIOOp>>(context);
@@ -119,7 +51,7 @@ void BufferOp::getCanonicalizationPatterns(RewritePatternSet &results,
 
 void IOPushOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                            MLIRContext *context) {
-  results.add<IOPushElim>(context);
+  results.add<DuplicateElim<IOPushOp>>(context);
 }
 
 void ConfigPLIOOp::getCanonicalizationPatterns(RewritePatternSet &results,
