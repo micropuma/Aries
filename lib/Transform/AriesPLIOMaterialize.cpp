1b31f7b0417e60ece216cb678239c6621ee5d655
JinmingZhuang <m13688033757@163.com>
2024-08-16 23:02:14 +0000

    [Transform][AriesPLIOMaterialize] Split IOPush/IOPop to new for loops

--- lib/Transform/AriesPLIOMaterialize.cpp
+++ lib/Transform/AriesPLIOMaterialize.cpp
@@ -162,7 +162,6 @@ private:
     */
     //////////////////////////////////////////////////////
     auto outerloop = band[0];
-    auto innerLoop = band[band.size()-1];
     SmallVector<int64_t, 4> sizesInt;
     unsigned rank = offsets.size();
     SmallVector<int64_t, 4> bufSizes;
@@ -225,33 +224,54 @@ private:
     }
 
     // Load data from external mem to L2 buffer
-    SmallVector<AffineForOp, 4> newLoops;
+    // Create loops for DMA Ops
+    SmallVector<AffineForOp, 4> newDMALoops;
     if (iopush)
       builder.setInsertionPoint(outerloop);
     else
       builder.setInsertionPointAfter(outerloop);
-    // Clone buffer related loops
     for (auto loop : loops){
-      auto newForOp 
+      auto newDMAForOp 
            = builder.create<AffineForOp>(loc,
                                          loop.getLowerBoundOperands(),
                                          loop.getLowerBoundMap(),
                                          loop.getUpperBoundOperands(),
                                          loop.getUpperBoundMap());
-      newLoops.push_back(newForOp);
-      builder.setInsertionPointToStart(newForOp.getBody());
+      newDMALoops.push_back(newDMAForOp);
+      builder.setInsertionPointToStart(newDMAForOp.getBody());
     }
+    // Create loops for IOPush/IOPop Ops
+    SmallVector<AffineForOp, 4> newIOLoops;
+    if (iopush)
+      builder.setInsertionPoint(outerloop);
+    else
+      builder.setInsertionPointAfter(outerloop);
+    for (auto loop : loops){
+      auto newIOForOp 
+           = builder.create<AffineForOp>(loc,
+                                         loop.getLowerBoundOperands(),
+                                         loop.getLowerBoundMap(),
+                                         loop.getUpperBoundOperands(),
+                                         loop.getUpperBoundMap());
+      newIOLoops.push_back(newIOForOp);
+      builder.setInsertionPointToStart(newIOForOp.getBody());
+    }
+
     // Create affine ApplyOps inside the innermost new loop
+    auto newInnerDMALoop = newDMALoops[newDMALoops.size()-1];
+    auto newInnerIOLoop = newIOLoops[newIOLoops.size()-1];
+    auto newInnerDMAYiled = newInnerDMALoop.getBody()->getTerminator();
+    auto newInnerIOYiled = newInnerIOLoop.getBody()->getTerminator();
     std::reverse(applyops.begin(), applyops.end());
     SmallVector<AffineApplyOp, 4> newApplyops;
     SmallVector<Value, 4> nestedOperands;
     SmallVector<unsigned, 4> ApplyOpIndex;
     for (auto applyop : applyops){
+      builder.setInsertionPoint(newInnerDMAYiled);
       auto newApplyop = builder.create<AffineApplyOp>(loc, 
                                                       applyop.getAffineMap(),
                                                       applyop.getOperands());
       newApplyops.push_back(newApplyop);
-      builder.setInsertionPointAfter(newApplyop);
       // Get operands that defined by another AffineApplyOp
       for (auto operand : applyop.getOperands()){
         auto definedOp = operand.getDefiningOp();
@@ -269,7 +289,6 @@ private:
         }
       }
     }
-    
     // Create local buffer offset for dma & IOPush/IOPop
     SmallVector<Value, 4> localOffsets;
     SmallVector<Value, 4> IOOffsets;
@@ -280,57 +299,64 @@ private:
       // Add ApplyOp for dma
       auto loop = loops[i];
       auto var = loop.getInductionVar();
+      builder.setInsertionPoint(newInnerDMAYiled);
       auto newApplyopDMA = builder.create<AffineApplyOp>(loc, map, var);
       localOffsets.push_back(newApplyopDMA);
       // Add ApplyOp for IOPush/IOPop
-      builder.setInsertionPointToStart(innerLoop.getBody());
-      loop = loops[i];
-      var = loop.getInductionVar();
-      auto newApplyop = builder.create<AffineApplyOp>(loc, map, var);
-      IOOffsets.push_back(newApplyop);
-      builder.setInsertionPointAfter(newApplyopDMA);
+      builder.setInsertionPoint(newInnerIOYiled);
+      auto newApplyopIO = builder.create<AffineApplyOp>(loc, map, var);
+      IOOffsets.push_back(newApplyopIO);
     }
     if (iopush){
       // Create dma op to load data from external mem to L2 buffer
       // Replace IOPush: Send data from L2 buffer to L1 buffer
+      builder.setInsertionPoint(newInnerDMAYiled);
       builder.create<DmaOp>(loc, src,      offsets,      sizes,      strides,
                              allocOp, localOffsets, localSizes, localStrides);
-      builder.setInsertionPoint(iopushOp);
+      builder.setInsertionPoint(newInnerIOYiled);
       builder.create<IOPushOp>(loc, allocOp, IOOffsets, localSizes, 
                                localStrides, dst);
-      iopushOp.erase();
     }else{
       // Create dma op to store data from L2 buffer to external mem
       // Replace IOPop: Receive data from L1 buffer to L2 buffer
+      builder.setInsertionPoint(newInnerDMAYiled);
       builder.create<DmaOp>(loc,allocOp, localOffsets, localSizes, localStrides,
                                 dst,     offsets,      sizes,      strides);
-      builder.setInsertionPoint(iopopOp);
+      builder.setInsertionPoint(newInnerIOYiled);
       builder.create<IOPopOp>(loc, src, allocOp, 
                               IOOffsets, localSizes, localStrides);
-      iopopOp.erase();
     }
 
-    // Update the loop variable used in AffineApply in the newInnerLoop
-    auto numVi = newLoops.size();
-    auto newInnerLoop = newLoops[newLoops.size()-1];
+    // Update the loop variable used in AffineApply in the newInnerDMALoop
+    auto numVi = newDMALoops.size();
     for (unsigned i = 0; i < numVi; ++i) {
       auto oldvi = loops[i].getInductionVar();
-      auto newvi = newLoops[i].getInductionVar();
+      auto newvi = newDMALoops[i].getInductionVar();
       oldvi.replaceUsesWithIf(newvi,[&](OpOperand &use){
-          return newInnerLoop->isProperAncestor(use.getOwner());
+          return newInnerDMALoop->isProperAncestor(use.getOwner());
       });
     }
 
-    // Update the Operands defined by AffineApply in the newInnerLoop
+    // Update the loop variable used in AffineApply in the newInnerIOLoop
+    numVi = newIOLoops.size();
+    for (unsigned i = 0; i < numVi; ++i) {
+      auto oldvi = loops[i].getInductionVar();
+      auto newvi = newIOLoops[i].getInductionVar();
+      oldvi.replaceUsesWithIf(newvi,[&](OpOperand &use){
+          return newInnerIOLoop->isProperAncestor(use.getOwner());
+      });
+    }
+
+    // Update the Operands defined by AffineApply in the newInnerDMALoop
     for (unsigned i = 0; i < nestedOperands.size(); i++) {
       auto oldNestedOperand = nestedOperands[i];
       auto newNestedOperand = newApplyops[ApplyOpIndex[i]];
       oldNestedOperand.replaceUsesWithIf(newNestedOperand,[&](OpOperand &use){
-          return newInnerLoop->isProperAncestor(use.getOwner());
+          return newInnerDMALoop->isProperAncestor(use.getOwner());
       });
     }
 
-    // Update the offsets of dma in the newInnerLoop
+    // Update the offsets of dma in the newInnerDMALoop
     for (unsigned i = 0; i < rank; i++) {
       auto oldOffset = offsets[i];
       auto definedOp = oldOffset.getDefiningOp();
@@ -346,7 +372,7 @@ private:
       unsigned index = std::distance(applyops.begin(), it);
       auto newOffset = newApplyops[index];
       oldOffset.replaceUsesWithIf(newOffset,[&](OpOperand &use){
-          return newInnerLoop->isProperAncestor(use.getOwner());
+          return newInnerDMALoop->isProperAncestor(use.getOwner());
       });
     }
     return WalkResult::advance();
@@ -375,6 +401,7 @@ private:
     }
 
     // Move AffineApplyOp to the innerLoop
+    auto outerLoop = band[0];
     auto innerLoop = band[band.size()-1];
     auto loopBody = innerLoop.getBody();
     SmallVector<AffineApplyOp, 6> applyOpsBefore;
@@ -399,6 +426,9 @@ private:
 
     if (flag == WalkResult::interrupt()) 
       return false;
+    
+    // After Processes IOPush/IOPop safe to erase outerLoop
+    outerLoop.erase();
       
     return true;
   }
