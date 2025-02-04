365b70240e41473925803d832b4a61050459321a
arclab <m13688033757@163.com>
2025-02-03 19:09:20 -0500

    [Transform][AriesBroadcastDetect] Merge DmaOps with same source together

--- lib/Transform/AriesBroadcastDetect.cpp
+++ lib/Transform/AriesBroadcastDetect.cpp
@@ -2,6 +2,7 @@
 #include "mlir/IR/Builders.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/Passes.h"
 #include "mlir/Dialect/Affine/Passes.h"
 #include "llvm/Support/Debug.h"
 #include "aries/Transform/Passes.h"
@@ -20,6 +21,11 @@ struct AriesBroadcastDetect
 public:
   void runOnOperation() override {
     auto mod = dyn_cast<ModuleOp>(getOperation());
+    PassManager pm(&getContext());
+    pm.addPass(createCSEPass());
+    pm.addPass(createCanonicalizerPass());
+    if (failed(pm.run(mod)))
+      signalPassFailure();
     if (!BroadcastDetect(mod))
       signalPassFailure();
   }
@@ -27,7 +33,52 @@ public:
 private:
   bool BroadcastDetect (ModuleOp mod) {
     auto builder = OpBuilder(mod);
-
+    auto loc = builder.getUnknownLoc();
+    // Tranverse all the adf.func
+    for (auto func : mod.getOps<FuncOp>()) {
+      if(!func->hasAttr("adf.func"))
+        continue;
+      SmallVector<DmaOp, 6> dmaOps;
+      func.walk([&](DmaOp op){
+        dmaOps.push_back(op);
+      });
+      // Record the index of dmaOps that has the same source
+      SmallVector<DmaOp, 6> eraseOps;
+      auto dmaNum = dmaOps.size();
+      for(unsigned i = 0; i < dmaNum; i++){
+        auto dma0 = dmaOps[i];
+        auto lastDmaOp = dma0;
+        SmallVector<Value, 6> dsts;
+        dsts.push_back(dma0.getDst());
+        for (unsigned j = i+1; j < dmaNum; j++) {
+          auto dma1 = dmaOps[j];
+          if (dma0.getSrc() == dma1.getSrc() &&
+              dma0.getSrcOffsets() == dma1.getSrcOffsets() &&
+              dma0.getSrcSizes() == dma1.getSrcSizes() &&
+              dma0.getSrcStrides() == dma1.getSrcStrides()){
+            lastDmaOp = dma1;
+            eraseOps.push_back(dma1);
+            dsts.push_back(dma1.getDst());
+          }
+        }
+        auto dstSize = dsts.size();
+        if(dstSize <= 1)
+          continue;
+        eraseOps.push_back(dma0);
+        // Create DmaBroadcastOp
+        builder.setInsertionPoint(lastDmaOp);
+        builder.create<DmaBroadcastOp>(loc, lastDmaOp.getSrc(),
+                                       lastDmaOp.getSrcOffsets(), 
+                                       lastDmaOp.getSrcSizes(),
+                                       lastDmaOp.getSrcStrides(), 
+                                       dsts, lastDmaOp.getDstOffsets(),
+                                       lastDmaOp.getDstSizes(),
+                                       lastDmaOp.getDstStrides());
+      }
+      // Erase the DmaOps
+      for(auto op: llvm::make_early_inc_range(eraseOps))
+        op.erase();
+    }
     return true;
   }
 
